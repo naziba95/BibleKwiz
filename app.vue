61856d024467a1d86b5e647d939069d18fbe22ba
Donenaziba Josiah <jdonenaziba@gmail.com>
2024-09-20 06:59:21 +0100

    modify app.vue

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+    <NuxtPage />
+  </template>
+  
+  <script setup>
+  import { onMounted } from 'vue'
+  import { useRouter } from 'vue-router'
+  
+  const router = useRouter()
+  
+  onMounted(() => {
+    if (router.currentRoute.value.path === '/') {
+      router.push('/login')
+    }
+  })
+  </script>
