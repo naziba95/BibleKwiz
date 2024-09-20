3cae6d7d85aa33251666eaa5a2088db339689d67
Donenaziba Josiah <jdonenaziba@gmail.com>
2024-09-20 05:40:05 +0100

    add app.vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  // pages: false,
+  ssr: false,
   modules: ['@nuxtjs/tailwindcss','@pinia/nuxt'],
   devtools: { enabled: false },
   runtimeConfig: {
